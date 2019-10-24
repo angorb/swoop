cb64bfaedea452bb712a5900a41bfc130cc78e37
Nick Brogna <nick@wemakepretty.com>
2019-10-24 13:41:04 -0600

    [fix] correct includes for phar creation

--- main.php
+++ main.php
@@ -1,6 +1,5 @@
 <?php
-define('ROOT_DIR', realpath(__DIR__ . "/"));
-require ROOT_DIR . '/vendor/autoload.php';
-require ROOT_DIR . '/class.Swoop.php';
+require 'vendor/autoload.php';
+require 'class.Swoop.php';
 error_reporting(E_ERROR);
 $main = new Swoop($argv);
